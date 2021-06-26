0e43b78bdf08b3f1fb36920eba77365ee4035f98
quadriniL <quadrinho.tiolu@gmail.com>
2021-06-26 13:27:19 -0300

    first-commit

---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu primeiro app! :D"),
+      ),
+      body: Container(
+        color: Colors.blueAccent,
+        child: Center(
+          child: Container(
+            color: Colors.amber,
+            width: 200,
+            height: 200,
+            child: Center(
+                child: Text(
+              "Contador:\n$count",
+              textAlign: TextAlign.center,
+            )),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
+    );
+  }
+}
